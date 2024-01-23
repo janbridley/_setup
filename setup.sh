f02a4c0065447d8606affb613b3f1d8126f4e056
janbridley <bradleyjenj@gmail.com>
2024-01-23 11:15:50 -0500

    Reordered functions

--- setup.sh
+++ setup.sh
@@ -1,22 +1,4 @@
-# Install micromamba
-"${SHELL}" <(curl -L micro.mamba.pm/install.sh)
-
-# Set default channel to conda-forge
-micromamba config append channels conda-forge
-
-# Create a new environment
-micromamba create -n 690py312 python=3.12
-micromamba activate 690py312
-
-# Install ovito python package
-micromamba install --strict-channel-priority -c https://conda.ovito.org -c conda-forge ovito
-
-# Install pyutils (private module)
-git clone https://github.com/janbridley/pyutils.git
-cd pyutils
-pip install .
-cd ..
-rm -rf pyutils
+#!/bin/bash
 
 # Set up function to ask for execution
 prompt_and_execute() {
@@ -31,6 +13,31 @@ prompt_and_execute() {
 # Attempt to change shell (optional)
 prompt_and_execute "Do you want to try to change shell?" "chsh $(which zsh)"
 
+
+# Install oh-my-zsh (optional)
+prompt_and_execute "Do you want to install oh-my-zsh?" "sh -c \'$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\'"
+
+
+# Install micromamba
+"${SHELL}" <(curl -L micro.mamba.pm/install.sh)
+
+# Set default channel to conda-forge
+micromamba config append channels conda-forge
+
+# Create a new environment
+micromamba create -n 690py312 python=3.12
+micromamba activate 690py312
+
+# Install ovito python package
+micromamba install --strict-channel-priority -c https://conda.ovito.org -c conda-forge ovito
+
+# Install pyutils (private module)
+#git clone https://github.com/janbridley/pyutils.git
+#cd pyutils
+#pip install .
+#cd ..
+#rm -rf pyutils
+
 # Add micromamba alias function to current shell
 mmfun='
 
@@ -38,8 +45,8 @@ function mm() {
     case "$1" in
         "c" )
             shift
-	    micromamba create -n "$1" "${@:2}"
-	    #[[ $# -ge 2 ]] && conda create -n "$2" "${@:3}" || echo "Usage: mm create <environment_name>"
+        micromamba create -n "$1" "${@:2}"
+        #[[ $# -ge 2 ]] && conda create -n "$2" "${@:3}" || echo "Usage: mm create <environment_name>"
             ;;
         "a" | "activate")
             shift
@@ -56,10 +63,10 @@ function mm() {
             shift
             micromamba update "$@"
             ;;
-	"ls" | "list")
-	    shift
-	    micromamba list "$@"
-	    ;;
+    "ls" | "list")
+        shift
+        micromamba list "$@"
+        ;;
         "rm" | "remove")
             shift
             micromamba remove "$@"
@@ -74,17 +81,12 @@ function mm() {
 read -p "Initialize mm mamba alias? (y=zshrc/n=bashrc): " choice
 
 if [ "$choice" == "y" ] || [ "$choice" == "Y" ]|| [ "$choice" == "zsh" ]; then
-	# Append the lines to ~/.zshrc
-	echo "$mmfun" >> ~/.zshrc
+    # Append the lines to ~/.zshrc
+    echo "$mmfun" >> ~/.zshrc
 else
     # Append the lines to ~/.bashrc
-	echo "$mmfun" >> ~/.bashrc
+    echo "$mmfun" >> ~/.bashrc
 fi
-
-
-# Install oh-my-zsh (optional)
-prompt_and_execute "Do you want to install oh-my-zsh?" "sh -c \'$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\'"
-
 
 echo "Setup complete :D"
 
